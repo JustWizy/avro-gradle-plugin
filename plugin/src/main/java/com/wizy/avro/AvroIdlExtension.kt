52466bf727db0e4788cb0f89db6099f8b2cd50f3
Ilia Dorokhin <idorokhin@gmail.com>
2025-08-16 17:22:51 +0300

    Add Avro schema generation Gradle plugin
    Implemented AvroIdlToSchemaTask and plugin extension for
    generating Avro schemas from IDL files. Added Gradle wrapper.

--- plugin/src/main/java/com/wizy/avro/AvroIdlExtension.kt
+++ plugin/src/main/java/com/wizy/avro/AvroIdlExtension.kt
@@ -2,34 +2,29 @@ package com.wizy.avro
 
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.ProjectLayout
-import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.ListProperty
 import org.gradle.api.provider.Property
 import java.io.File
 import javax.inject.Inject
 
 /**
- * Пользовательская конфигурация плагина:
- *  - sourceDirs: откуда брать .avdl
- *  - outputBaseDir: куда генерить .avsc/.pr
- *  - writeProtocolJson: генерировать ли .pr (protocol JSON)
- */
-open class AvroIdlExtension @Inject constructor(
-    objects: ObjectFactory,
+ * Пользовательская конфигурация плагина (куда складывать .avdl, куда генерировать .avsc).
+ *  */
+abstract class AvroIdlExtension @Inject constructor(
     layout: ProjectLayout
 )
 {
 
-    /** Список директорий с .avdl */
-    val sourceDirs: ListProperty<File> =
-        objects.listProperty(File::class.java).convention(emptyList())
+    abstract val sourceDirs: ListProperty<File>
 
-    /** Базовая директория генерации .avsc/.pr */
-    val outputBaseDir: DirectoryProperty =
-        objects.directoryProperty()
-            .convention(layout.buildDirectory.dir("generated/avro"))
+    abstract val outputBaseDir: DirectoryProperty
 
-    /** Генерировать protocol JSON (.pr) */
-    val writeProtocolJson: Property<Boolean> =
-        objects.property(Boolean::class.java).convention(true)
+    abstract val writeProtocolJson: Property<Boolean>
+
+    init
+    {
+        sourceDirs.convention(emptyList())
+        outputBaseDir.convention(layout.buildDirectory.dir("generated/avro"))
+        writeProtocolJson.convention(true)
+    }
 }
--- plugin/src/main/java/com/wizy/avro/AvroIdlToSchemaPlugin.kt
+++ plugin/src/main/java/com/wizy/avro/AvroIdlToSchemaPlugin.kt
@@ -17,12 +17,7 @@ class AvroIdlToSchemaPlugin : Plugin<Project>
 
     override fun apply(project: Project)
     {
-        val ext = project.extensions.create(
-            "avroIdl",
-            AvroIdlExtension::class.java,
-            project.objects,
-            project.layout
-        )
+        val ext = project.extensions.create("avroIdl", AvroIdlExtension::class.java)
 
         project.plugins.withId("java") { configureForJava(project, ext) }
         project.plugins.withId("java-library") { configureForJava(project, ext) }
@@ -58,7 +53,7 @@ class AvroIdlToSchemaPlugin : Plugin<Project>
 
             resources.srcDir(baseForSet)
 
-
+            
             project.tasks.named<Copy>(processResourcesTaskName).configure {
                 dependsOn(genTask)
             }
